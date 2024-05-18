da8ad9db32a0add1209018382508b7277c699e34
Fabio Miguel <fabiomnascimento05@gmail.com>
2024-05-18 13:58:43 -0300

    Adicionado traducao da pagina e resposividade para menuMobile

---
+++ JS/dublagem.js
@@ -0,0 +1,55 @@
+var translations = {
+    'en': {
+        'WhoAmI': 'Who am I',
+        'WhoAmIText': 'Since childhood, I have maintained a strong involvement with the world of technology, awakening an interest in programming from an early age. Over the years, I have developed skills in creating small codes and functions. Currently, at 19 years old, I am looking for opportunities in the technology market. I have a great appreciation for video games and a deep interest in web development, especially using <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>React</b >, in addition to software development with <b>Java</b>, <b>Spring Boot</b> and <b>C#</b>',
+        'Language': 'Language',
+        'LanguageText': 'These are some of the <b>languages</b> and <b>frameworks</b> that I have professionalized over the years</span>',
+        'Projects': 'Projects',
+        'ProjectsText': 'In this section, you will find a selection of my most recent projects, as well as those that I consider the best and most representative of my work',
+        'BoxProjectName': 'Cool Project',
+        'BoxProjectText': 'Project description, because this is a very cool, fun, and super interactive project',
+        'LikedWhatYouSaw?': 'Did you like what you saw?',
+        'LikedWhatYouSaw?Text': 'If you like my work and want to contact me, feel free to send me a message. I am always open to new opportunities and challenges',
+        'GetInTouch': 'Get in Touch',
+        'Name': 'Name',
+        'Phone': 'Phone',
+        'Message': 'Message',
+        'Submit': 'Submit',
+        'NavLanguage': 'Languages',
+        'NavProjects': 'Projects',
+        'NavContact': 'Contact',
+        'Repository': '<i class="fab fa-github" </i> <b>Repository</b>',
+    },
+    'pt': {
+        'WhoAmI': 'Quem eu sou',
+        'WhoAmIText': 'Desde a infância, tenho mantido um forte envolvimento com o universo da tecnologia, despertando um interesse pela programação desde cedo. Ao longo dos anos, desenvolvi habilidades na criação de pequenos códigos e funções. Atualmente, aos 19 anos, estou buscando oportunidades no mercado de tecnologia. Tenho um grande apreço por videogames e um profundo interesse em desenvolvimento web, especialmente utilizando <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>React</b>, além do desenvolvimento de software com <b>Java</b>, <b>Spring Boot</b> e <b>C#</b>',
+        'Language' : 'Linguagens',
+        'LanguageText': 'Essas são algumas das <b>linguagens</b> e <b>frameworks</b> que tenho me profissionalizado ao longo dos anos</span>',
+        'Projects': 'Projetos',
+        'ProjectsText': 'Nesta seção, você encontrará uma seleção dos meus projetos mais recentes, bem como aqueles que considero os melhores e mais representativos do meu trabalho',
+        'BoxProjectName': 'Projeto Bacana',
+        'BoxProjectText': 'Descrição do projeto, pois esse é um projeto muito bacana, muito legal e interativo, super duper bacana',
+        'LikedWhatYouSaw?' : 'Gostou do que viu?',
+        'LikedWhatYouSaw?Text' : 'Se você gostou do meu trabalho e deseja entrar em contato comigo, sinta-se à vontade para me enviar uma mensagem. Estou sempre aberto a novas oportunidades e desafios',
+        'GetInTouch' : 'Entre em Contato',
+        'Name' : 'Nome',
+        'Phone' : 'Telefone',
+        'Message' : 'Mensagem',
+        'Submit' : 'Enviar',
+        'NavLanguage': 'Linguagens',
+        'NavProjects': 'Projetos',
+        'NavContact': 'Contato',
+        'Repository': '<i class="fab fa-github" ></i> <b>Repositório</b>',
+    }
+};
+
+function changeLanguage(lang) {
+    var elements = document.querySelectorAll('[data-translate]');
+    elements.forEach(function (element) {
+        var key = element.getAttribute('data-translate');
+        if (translations[lang] && translations[lang][key]) {
+            element.innerHTML = translations[lang][key];
+        }
+    });
+}
+
--- JS/nav.js
+++ JS/nav.js
@@ -1,56 +1,3 @@
-// function closeAllDropdowns(exceptId) {
-//     ["languageDropdown", "themeDropdown"].forEach(function(id) {
-//         if (id !== exceptId) {
-//             var dropdown = document.getElementById(id);
-//             if (dropdown) {
-//                 dropdown.style.display = "none";
-//             }
-//         }
-//     });
-// }
-
-// document.getElementById("settings").addEventListener("click", function() {
-//     var popup = document.getElementById("settingsPopup");
-//     if (popup.style.display === "none") {
-//         popup.style.display = "block";
-//     } else {
-//         popup.style.display = "none";
-//     }
-// });
-
-// document.getElementById("closePopup").addEventListener("click", function() {
-//     document.getElementById("settingsPopup").style.display = "none";
-// });
-
-// document.getElementById("languageIcon").addEventListener("click", function() {
-//     closeAllDropdowns("languageDropdown");
-//     var dropdown = document.getElementById("languageDropdown");
-//     if (dropdown.style.display === "none") {
-//         dropdown.style.display = "block";
-//     } else {
-//         dropdown.style.display = "none";
-//     }
-// });
-
-// Array.from(document.querySelectorAll("#languageDropdown a")).forEach(function(link) {
-//     link.addEventListener("click", function(e) {
-//         e.preventDefault();
-//         var language = this.getAttribute("data-value");
-//         console.log("Selected language: " + language);
-//     });
-// });
-
-// document.getElementById("themeIcon").addEventListener("click", function() {
-//     closeAllDropdowns("themeDropdown");
-//     var dropdown = document.getElementById("themeDropdown");
-//     if (dropdown.style.display === "none") {
-//         dropdown.style.display = "block";
-//     } else {
-//         dropdown.style.display = "none";
-//     }
-// });
-
-
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
@@ -78,4 +25,46 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+window.addEventListener('click', function (e) {
+    if (!primaryNav.contains(e.target) && !navToggle.contains(e.target)) {
+        primaryNav.setAttribute('data-visible', 'false');
+        navToggle.setAttribute("aria-expanded", "false");
+    }
+});
 
+
+
+
+// Get all custom select elements
+let customSelects = document.querySelectorAll('.custom-select');
+
+// Attach click event listeners to each custom select
+customSelects.forEach(function (select) {
+    let selectSelected = select.querySelector('.select-selected');
+    let selectItems = select.querySelector('.select-items');
+    let options = selectItems.querySelectorAll('div');
+
+    // Toggle the dropdown visibility when the select box is clicked
+    selectSelected.addEventListener('click', function () {
+        if (selectItems.style.display === 'block') {
+            selectItems.style.display = 'none';
+        } else {
+            selectItems.style.display = 'block';
+        }
+    });
+
+    // Set the selected option, hide the dropdown, and change language when an option is clicked
+    options.forEach(function (option) {
+        option.addEventListener('click', function () {
+            selectItems.style.display = 'none';
+            changeLanguage(this.id); // Call the changeLanguage function with the id of the clicked option
+        });
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener('click', function (e) {
+        if (!select.contains(e.target)) {
+            selectItems.style.display = 'none';
+        }
+    });
+});
