569d244373b6e28548bb903af18e144332b57a5f
Fabio Miguel <fabiomnascimento05@gmail.com>
2024-09-02 00:53:49 -0300

    chore: Update project images and paths

--- JS/dublagem.js
+++ JS/dublagem.js
@@ -1,6 +1,5 @@
 var translations = {
     'en': {
-        // Traduções existentes
         'WhoAmI': 'Who am I',
         'WhoAmIText': 'Since childhood, I have maintained a strong involvement with the world of technology, awakening an interest in programming from an early age. Over the years, I have developed skills in creating small codes and functions. Currently, at 19 years old, I am looking for opportunities in the technology market. I have a great appreciation for video games and a deep interest in web development, especially using <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>React</b>, in addition to software development with <b>Java</b>, <b>Spring Boot</b> and <b>C#</b>',
         'Language': 'Language',
@@ -22,7 +21,6 @@ var translations = {
         'footer-credits': '© 2024 <b>Fabio Miguel Nascimento</b>. All rights reserved',
         'fullstack': 'Full Stack Developer',
 
-        // Novas traduções para projetos
         'project_name_1': 'Online Store',
         'project_description_1': 'This project is a replica of an e-commerce store for selling various products. It relies on JavaScript to perform the functions of fetch, product categories (in production), shopping cart (in production), product filtering (in production)',
         'project_name_2': 'Animal World',
@@ -30,24 +28,20 @@ var translations = {
         'project_name_3': 'Landing Pages',
         'project_description_3': 'Here I demonstrate some landing pages I made in Figma and Photoshop. These are launch pages for people and small causes.',
 
-        // Status
         'status_em_andamento': 'In Progress',
         'status_concluído': 'Completed',
 
-        // Tecnologias
         'tech_html': 'HTML',
         'tech_css': 'CSS',
         'tech_javascript': 'JavaScript',
         'tech_figma': 'Figma',
         'tech_photoshop': 'Photoshop',
 
-        // Links
         'link_github': 'GitHub',
         'link_site': 'Website',
         'link_figma': 'Figma'
     },
     'pt': {
-        // Traduções existentes
         'WhoAmI': 'Quem eu sou',
         'WhoAmIText': 'Desde a infância, tenho mantido um forte envolvimento com o universo da tecnologia, despertando um interesse pela programação desde cedo. Ao longo dos anos, desenvolvi habilidades na criação de pequenos códigos e funções. Atualmente, aos 19 anos, estou buscando oportunidades no mercado de tecnologia. Tenho um grande apreço por videogames e um profundo interesse em desenvolvimento web, especialmente utilizando <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>React</b>, além do desenvolvimento de software com <b>Java</b>, <b>Spring Boot</b> e <b>C#</b>',
         'Language' : 'Linguagens',
@@ -69,26 +63,21 @@ var translations = {
         'footer-credits' : '© 2024 <b>Fábio Miguel Nascimento</b>. Todos os direitos reservados',
         'fullstack': 'Desenvolvedor Full Stack',
 
-        // Novas traduções para projetos
         'project_name_1': 'Loja Online',
         'project_description_1': 'Este projeto é uma réplica de uma loja de comércio eletrônico para venda de diversos produtos. Ele conta com JavaScript para realizar as funções de fetch, categorias de produtos (em produção), carrinho de compras (em produção), filtragem de produtos (em produção)',
         'project_name_2': 'Mundo Animal',
         'project_description_2': 'Este projeto não passa de uma simples Landing Page que utiliza algumas funções como shape-outside e clip path. Este projeto mostra um pouco sobre a minha qualidade de design e como eu internalizo isso no código.',
         'project_name_3': 'Landing Pages',
         'project_description_3': 'Aqui demonstro algumas landing pages que fiz no Figma e Photoshop. São páginas de lançamento para pessoas e pequenas causas.',
-
-        // Status
         'status_em_andamento': 'Em andamento',
         'status_concluído': 'Concluído',
 
-        // Tecnologias
         'tech_html': 'HTML',
         'tech_css': 'CSS',
         'tech_javascript': 'JavaScript',
         'tech_figma': 'Figma',
         'tech_photoshop': 'Photoshop',
 
-        // Links
         'link_github': 'GitHub',
         'link_site': 'Site',
         'link_figma': 'Figma'
--- JS/projetos.js
+++ JS/projetos.js
@@ -1,107 +1,137 @@
 // Função para carregar os dados do JSON
 async function carregarProjetos() {
-    try {
-        const response = await fetch('data/projetos.json');
-        const data = await response.json();
-        return data.categorias;
-    } catch (error) {
-        console.error('Erro ao carregar os projetos:', error);
-    }
+  try {
+    const response = await fetch("data/projetos.json");
+    const data = await response.json();
+    return data.categorias;
+  } catch (error) {
+    console.error("Erro ao carregar os projetos:", error);
+  }
 }
 
 function criarHTMLProjeto(projeto, index) {
-    const direcao = index % 2 === 0 ? 'esquerda' : 'direita';
-   
-    function getIconPath(linguagem) {
-        const nomeArquivo = linguagem.toLowerCase().replace(/[^a-z0-9]/g, '') + '.svg';
-        return `./icons/${nomeArquivo}`;
-    }
+  const direcao = index % 2 === 0 ? "esquerda" : "direita";
 
-    function getStatusEmoji(status) {
-        switch (status.toLowerCase()) {
-            case 'em andamento':
-                return '🚧';
-            case 'concluído':
-                return '✅';
-            default:
-                return '❓';
-        }
-    }
+  function getIconPath(linguagem) {
+    const nomeArquivo =
+      linguagem.toLowerCase().replace(/[^a-z0-9]/g, "") + ".svg";
+    return `./icons/${nomeArquivo}`;
+  }
 
-    const imageOrIframe = projeto.isFigma
-        ? `<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="100%" src="${projeto.figmaEmbed}" allowfullscreen></iframe>`
-        : `<a href="${projeto.links.find(link => link.nome === 'Site')?.url || '#'}" target="_blank">
+  function getStatusEmoji(status) {
+    switch (status.toLowerCase()) {
+      case "em andamento":
+        return "🚧";
+      case "concluído":
+        return "✅";
+      default:
+        return "❓";
+    }
+  }
+
+  const imageOrIframe = projeto.isFigma
+    ? `<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="100%" height="100%" src="${projeto.figmaEmbed}" allowfullscreen></iframe>`
+    : `<a href="${
+        projeto.links.find((link) => link.nome === "Site")?.url || "#"
+      }" target="_blank">
                <img src="${projeto.imagem}" alt="${projeto.nome}">
            </a>`;
 
-    return `
-    <div class="box ${direcao} ${projeto.nome.replace(/\s+/g, '')}">
-        <div class="image">
-            ${imageOrIframe}
+  return `
+<div class="box ${direcao} ${projeto.nome.replace(/\s+/g, "")}">
+    <div class="image">
+        ${imageOrIframe}
+    </div>
+    <div class="detalhes">
+        <div class="status-emoji tooltip">
+            ${getStatusEmoji(projeto.status)}
+            <span class="tooltiptext" data-translate="status_${projeto.status
+              .toLowerCase()
+              .replace(/\s+/g, "_")}">${projeto.status}</span>
         </div>
-        <div class="detalhes">
-            <div class="status-emoji tooltip">
-                ${getStatusEmoji(projeto.status)}
-                <span class="tooltiptext" data-translate="status_${projeto.status.toLowerCase().replace(/\s+/g, '_')}">${projeto.status}</span>
-            </div>
-            <div class="nome">
-                <h1 data-translate="project_name_${projeto.id}">${projeto.nome}</h1>
-            </div>
-            <div class="descricao">
-                <p data-translate="project_description_${projeto.id}">${projeto.descricao}</p>
-            </div>
-            <ul class="techstack">
-                ${projeto.linguagens ? projeto.linguagens.map(lang =>
-                    `<li class="tooltip">
-                        <img src="${getIconPath(lang)}" alt="${lang}" class="icon">
-                        <span class="tooltiptext" data-translate="tech_${lang.toLowerCase().replace(/\s+/g, '_')}">${lang}</span>
-                    </li>`
-                ).join('') : ''}
-            </ul>
-            <div class="btns">
-                ${projeto.links.map(link =>
-                    `<a href="${link.url}" target="_blank" class="projetos-botao">
-                        <i class="${link.nome.toLowerCase() === 'site' ? 'fas fa-play' : 'fab fa-' + link.nome.toLowerCase()} icon"></i>
-                        <span data-translate="link_${link.nome.toLowerCase()}">${link.nome}</span>
-                    </a>`
-                ).join('')}
-            </div>
+        <div class="nome">
+            <h1 data-translate="project_name_${projeto.id}">${projeto.nome}</h1>
+        </div>
+        <div class="descricao">
+            <p data-translate="project_description_${projeto.id}">${
+    projeto.descricao
+  }</p>
+        </div>
+        <ul class="techstack">
+            ${
+              projeto.linguagens
+                ? projeto.linguagens
+                    .map(
+                      (lang) =>
+                        `<li class="tooltip">
+                    <img src="${getIconPath(lang)}" alt="${lang}" class="icon">
+                    <span class="tooltiptext" data-translate="tech_${lang
+                      .toLowerCase()
+                      .replace(/\s+/g, "_")}">${lang}</span>
+                </li>`
+                    )
+                    .join("")
+                : ""
+            }
+        </ul>
+        <div class="btns">
+            ${projeto.links
+              .map(
+                (link) =>
+                  `<a href="${
+                    link.url
+                  }" target="_blank" class="projetos-botao" aria-label="${
+                    link.nome
+                  }">
+                    <i class="${
+                      link.nome.toLowerCase() === "site"
+                        ? "fas fa-play"
+                        : "fab fa-" + link.nome.toLowerCase()
+                    } icon"></i>
+                    <span data-translate="link_${link.nome.toLowerCase()}">${
+                    link.nome
+                  }</span>
+                </a>`
+              )
+              .join("")}
         </div>
     </div>
+</div>
+
     `;
 }
 
 // Função para renderizar todos os projetos
 async function renderizarProjetos() {
-    const categorias = await carregarProjetos();
-    const container = document.querySelector('.container');
-    
-    if (!container) {
-        console.error('Elemento .container não encontrado');
-        return;
-    }
+  const categorias = await carregarProjetos();
+  const container = document.querySelector(".container");
 
-    if (!categorias) {
-        console.error('Não foi possível carregar os projetos');
-        return;
-    }
+  if (!container) {
+    console.error("Elemento .container não encontrado");
+    return;
+  }
 
-    let todosOsProjetos = categorias.flatMap(categoria => categoria.projetos);
+  if (!categorias) {
+    console.error("Não foi possível carregar os projetos");
+    return;
+  }
 
-    todosOsProjetos.forEach((projeto, index) => {
-        const projetoHTML = criarHTMLProjeto(projeto, index);
-        container.innerHTML += projetoHTML;
-    });
+  let todosOsProjetos = categorias.flatMap((categoria) => categoria.projetos);
+
+  todosOsProjetos.forEach((projeto, index) => {
+    const projetoHTML = criarHTMLProjeto(projeto, index);
+    container.innerHTML += projetoHTML;
+  });
 }
 
-// Inicialização 
+// Inicialização
 function inicializar() {
-    renderizarProjetos();
+  renderizarProjetos();
 }
 
 // Esperar o DOM carregar completamente antes de inicializar
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', inicializar);
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", inicializar);
 } else {
-    inicializar();
+  inicializar();
 }
